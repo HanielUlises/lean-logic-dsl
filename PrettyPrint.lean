68476c53a5a9d01486664b1034afe99a15263639
HanielUlises <hanielulises2003@gmail.com>
2026-04-01 23:22:51 -0600

    Precedence logic to avoid ambiguity

--- PrettyPrint.lean
+++ PrettyPrint.lean
@@ -27,14 +27,27 @@ def Formula.toString (φ : Formula) : String := φ.toStringPrec 0
 instance : ToString Formula where
   toString := Formula.toString
 
-def Formula.toLatex : Formula → String
+-- Precedence-aware LaTeX renderer (mirrors toStringPrec to avoid ambiguity)
+def Formula.toLatexPrec (prec : Nat) : Formula → String
   | .var x    => x
   | .top      => "\\top"
   | .bot      => "\\bot"
-  | .neg φ    => "\\neg " ++ φ.toLatex
-  | .conj φ ψ => φ.toLatex ++ " \\land " ++ ψ.toLatex
-  | .disj φ ψ => φ.toLatex ++ " \\lor " ++ ψ.toLatex
-  | .impl φ ψ => φ.toLatex ++ " \\to " ++ ψ.toLatex
-  | .bimpl φ ψ => φ.toLatex ++ " \\leftrightarrow " ++ ψ.toLatex
+  | .neg φ    =>
+      let inner := "\\neg " ++ φ.toLatexPrec 80
+      if prec > 75 then "(" ++ inner ++ ")" else inner
+  | .conj φ ψ =>
+      let inner := φ.toLatexPrec 65 ++ " \\land " ++ ψ.toLatexPrec 66
+      if prec > 65 then "(" ++ inner ++ ")" else inner
+  | .disj φ ψ =>
+      let inner := φ.toLatexPrec 60 ++ " \\lor " ++ ψ.toLatexPrec 61
+      if prec > 60 then "(" ++ inner ++ ")" else inner
+  | .impl φ ψ =>
+      let inner := φ.toLatexPrec 56 ++ " \\to " ++ ψ.toLatexPrec 55
+      if prec > 55 then "(" ++ inner ++ ")" else inner
+  | .bimpl φ ψ =>
+      let inner := φ.toLatexPrec 50 ++ " \\leftrightarrow " ++ ψ.toLatexPrec 51
+      if prec > 50 then "(" ++ inner ++ ")" else inner
+
+def Formula.toLatex (φ : Formula) : String := φ.toLatexPrec 0
 
 end PropLogicDSL
